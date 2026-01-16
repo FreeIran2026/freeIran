3138de3d133ade37424aeab685e2cfbae0e9c5e6
Amirhossein Vatani <amirhossein.vatani@adesso-insurance-solutions.de>
2026-01-16 16:16:37 +0100

    complate report page

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Funktion zum Hinzufügen von Report-Links
+function addReportLink(appName, url, username) {
+    // Icon-Mapping für verschiedene Apps
+    const icons = {
+        'Instagram': '📷',
+        'Twitter': 'X',
+  
+        // Füge mehr hinzu, falls nötig
+    };
+
+    // Standard-Icon, falls App nicht bekannt
+    const icon = icons[appName] || '📱';
+
+    // HTML für den Link generieren
+    const linkHtml = `
+        <a class="btn" href="${url}" target="_blank" onclick="showCheckmark(this); return false;">
+          <div class="left">
+            <div class="icon">${icon}</div>
+            <div>
+              <div class="label">${appName}</div>
+              <div class="value">${username}</div>
+            </div>
+          </div>
+          <div class="arrow">›</div>
+        </a>
+        <span class="checkmark" style="display:none;">✔️</span>
+    `;
+
+    // Link zur .links-Div hinzufügen
+    const linksContainer = document.getElementById('links');
+    if (linksContainer) {
+        linksContainer.innerHTML += linkHtml;
+    }
+}
+
+// Funktion zum Anzeigen des Häkchens und Öffnen des Links
+function showCheckmark(el) {
+    el.nextSibling.style.display = 'inline';
+    window.open(el.href, '_blank');
+}
